846cb1a8a0250f0eb9834c79cf870b84d496c232
heymonicakay <hey.monicakay@gmail.com>
2020-07-30 13:45:27 -0500

    added container for future logo in nav bar. formatted navbar css file slightly differently.

---
+++ scripts/logo/Logo.js
@@ -0,0 +1,11 @@
+const logoTarget = document.querySelector(".logo--container")
+const eventHub = document.querySelector(".header)
+
+eventHub.addEventListener
+export const Logo = () => {
+      
+      logoTarget.innerHTML = `
+      <img class="heading__image image--logo image" src="./images/tf-logo.png" alt="Truncheons & Flagons Logo" />
+      `
+      
+}
