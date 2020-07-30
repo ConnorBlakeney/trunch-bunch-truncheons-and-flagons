33687aa99719d8644d45f9bd55e2e1fe4cb9da76
mgjeffries <gib.jeffries@gmail.com>
2020-07-30 15:54:09 -0500

    fix players

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { ScoreList } from "./score/ScoreList.js";
 import { listTeams } from "./teams/TeamList.js";
-
+import { PlayerList } from "./player/PlayerList.js";
 
 ScoreList()
 listTeams()
+PlayerList()
--- scripts/score/ScoreList.js
+++ scripts/score/ScoreList.js
@@ -10,7 +10,6 @@ const render = (scores) => {
           scoreListHTML += scoreHTMLConverter(scoreObject)
       })
 
-  console.log(scoreListHTML)
 
       scoreListTarget.innerHTML = `
             <h2 class="heading heading--scores">
@@ -23,11 +22,9 @@ const render = (scores) => {
   }
 
 export const ScoreList = () => { 
-     console.log("SCORE LIST WAS INVOKED")   
       getScores()
             .then(() => {
                   const scores = useScores()
-            console.log(scores, "TEST SCORES RENDER")
                   render(scores)
             })
   }
