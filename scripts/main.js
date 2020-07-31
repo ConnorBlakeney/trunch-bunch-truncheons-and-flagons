bb8603b6a8c1b171ce06096e90cfe852f5e43f30
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-31 14:25:28 -0500

    added players to DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
-import { ScoreList } from "./score/ScoreList.js";
-import { listTeams } from "./teams/TeamList.js";
-import { PlayerList } from "./player/PlayerList.js";
-import { TeamForm } from "./teams/TeamForm.js"
+import {ScoreList} from "./score/ScoreList.js"
+import {listTeams} from "./teams/TeamList.js"
+import {PlayerList} from "./player/PlayerList.js"
+import {TeamForm} from "./teams/TeamForm.js"
+import {PlayerForm} from "./player/PlayerForm.js"
 
 ScoreList()
 listTeams()
 PlayerList()
 TeamForm()
+PlayerForm()
---
+++ scripts/player/PlayerForm.js
@@ -0,0 +1,38 @@
+import {savePlayer} from "./PlayerProvider.js"
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".create-player-form--container")
+
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.id === "playerButton") {
+    const playerNameFirst = document.querySelector("#playerNameFirst")
+    const playerNameLast = document.querySelector("#playerNameLast")
+    const countryOrigin = document.querySelector("#countryOrigin")
+
+    // Make a new object representation of a note
+    const newPlayer = {
+      nameFirst: playerNameFirst.value,
+      nameLast: playerNameLast.value,
+      countryOrigin: countryOrigin.value,
+    }
+
+    // Change API state and application state
+    savePlayer(newPlayer)
+  }
+})
+
+const render = () => {
+  contentTarget.innerHTML = `
+        <section class="playerForm">
+            <input type="text" id="playerNameFirst" placeholder="Enter Player First Name" />
+            <input type="text" id="playerNameLast" placeholder="Enter Player Last Name" />
+            <input type="text" id="countryOrigin" placeholder="Enter Player Country of Origin" />
+            <button id="playerButton">Create Player</button>
+        </section>
+    `
+}
+
+export const PlayerForm = () => {
+  render()
+}
