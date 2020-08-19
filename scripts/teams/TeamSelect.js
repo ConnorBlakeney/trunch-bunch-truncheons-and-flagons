e4fd4c07886d0865848608d92d36b0321b5f89ce
mgjeffries <gib.jeffries@gmail.com>
2020-08-19 16:12:42 -0500

    filter out full teams from team select

--- scripts/teams/TeamSelect.js
+++ scripts/teams/TeamSelect.js
@@ -1,14 +1,19 @@
 import {useTeams, getTeams} from "./TeamProvider.js"
+import {getPlayers, usePlayers} from "../player/PlayerProvider.js";
 
 const contentTarget = document.querySelector(".teamSelectContainer")
 const eventHub = document.querySelector(".container")
+
+let players = []
+let teams = []
+
 
 eventHub.addEventListener("teamStateChanged", () => {
   const allTeams = useTeams()
   render(allTeams)
 })
 
-const render = (teams) => {
+const render = () => {
   contentTarget.innerHTML = `
         <select class="dropdown" id="teamSelect">
             <option value="0">Please select a team</option>
@@ -22,8 +27,24 @@ const render = (teams) => {
 }
 
 export const TeamSelect = () => {
-  getTeams().then(() => {
-    const allTeams = useTeams()
-    render(allTeams)
+  getTeams()
+    .then(getPlayers)
+    .then(() => {
+      players = usePlayers()
+      teams = useTeams()
+      filterOutFullTeams()
+      render()
+  })
+}
+
+const filterOutFullTeams = () => {
+  teams = teams.filter( team => {
+    let playerCount = 0
+    players.forEach( player => {
+      if( player.teamId === team.id) {
+        playerCount++
+      }
+    })
+    return playerCount < 3
   })
 }
