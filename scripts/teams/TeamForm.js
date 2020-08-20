0afb9494d54842dee25e0a97ded6736b91231854
Connor Blakeney <connorblakeney@yahoo.com>
2020-08-20 15:13:14 -0500

    fixed teamform to validate create team button

--- scripts/teams/TeamForm.js
+++ scripts/teams/TeamForm.js
@@ -7,7 +7,12 @@ const contentTarget = document.querySelector(".create-team-form--container")
 eventHub.addEventListener("click", (clickEvent) => {
   if (clickEvent.target.id === "teamButton") {
     const teamName = document.querySelector("#teamName")
-    if(teamName.length <= 11) {
+    if (teamName.value.length === 0) {
+        window.alert("Please enter a team name!")
+    }
+      else if (teamName.value.length >= 11){
+        window.alert("Character limit for team name is 11. Please follow these rules! ")
+      } else {
       const newTeam = {
         name: teamName.value,
         timestamp: Date.now(),
@@ -17,12 +22,10 @@ eventHub.addEventListener("click", (clickEvent) => {
       saveTeam(newTeam)
       render()
     }
-      else {
-        window.alert("Character limit for team name is 11. Please follow these rules! ")
-      }
     // Make a new object representation of a note
   }
 })
+
 
 const render = () => {
   contentTarget.innerHTML = `
